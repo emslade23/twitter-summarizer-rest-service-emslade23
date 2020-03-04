cf17a87c291bba96e45ade028938acfe532e8d46
Elizabeth Slade <elizabethslade23@gmail.com>
2020-03-04 16:18:54 -0500

    commented out test file

--- test_app.py
+++ test_app.py
@@ -6,52 +6,51 @@ import shutil
 import threading
 
 
-flag = False
+# flag = False
  
 
-if os.path.isfile(os.getcwd() + "/keys") == True:
-    flag = True
-    shutil.copy('keys', 'keys.py')
-    from keys import *
+# if os.path.isfile(os.getcwd() + "/keys") == True:
+#     flag = True
+#     shutil.copy('keys', 'keys.py')
+#     from keys import *
 
-def testCreateVideoGivenTwitterHandleAndKeys():
-    globalProcesses.init()
-    twitterHandle = "@elonmusk"
-    tweetNumber = 5
-    globalProcesses.twitterCompletedTasks[twitterHandle] = "Not Completed"
+# def testCreateVideoGivenTwitterHandleAndKeys():
+#     globalProcesses.init()
+#     twitterHandle = "@elonmusk"
+#     tweetNumber = 5
+#     globalProcesses.twitterCompletedTasks[twitterHandle] = "Not Completed"
 
-    if flag:
-        directory = "musky"
-        userTweetsObj = userTweets(consumer_key, consumer_secret, access_token, access_token_secret, twitterHandle, tweetNumber, directory)
-        userTweetsObj.getTweetData()
-        userTweetsObj.makeDirectory()
+#     if flag:
+#         directory = "musky"
+#         userTweetsObj = userTweets(consumer_key, consumer_secret, access_token, access_token_secret, twitterHandle, tweetNumber)
+#         userTweetsObj.getTweetData()
 
-        assert len(userTweetsObj.textOfTweets) > 0
-        assert len(userTweetsObj.imagesOfTweets) > 0
+#         assert len(userTweetsObj.textOfTweets) > 0
+#         assert len(userTweetsObj.imagesOfTweets) > 0
 
-        userTweetsObj.createImagesofTweets()
-        assert globalProcesses.twitterCompletedTasks[twitterHandle] == "50% Complete"
+#         userTweetsObj.createImagesofTweets()
+#         assert globalProcesses.twitterCompletedTasks[twitterHandle] == "50% Complete"
 
-        userTweetsObj.convertImagestoVideo()
-        assert os.path.isfile(os.getcwd() + '/' + userTweetsObj.directory + '/test2.mp4') == True
+#         userTweetsObj.convertImagestoVideo()
+#         assert os.path.isfile(os.getcwd() + '/' + userTweetsObj.directory + '/test2.mp4') == True
 
-    else:
-        directory = "apiExampleVideo"
-        userTweetsObj = userTweets(0, 0, 0, 0, twitterHandle, tweetNumber, directory)
-        userTweetsObj.convertImagestoVideo()
-        assert os.path.isfile(os.getcwd() + '/' + userTweetsObj.directory + '/test2.mp4') == True
+#     else:
+#         directory = "apiExampleVideo"
+#         userTweetsObj = userTweets(0, 0, 0, 0, twitterHandle, tweetNumber, directory)
+#         userTweetsObj.convertImagestoVideo()
+#         assert os.path.isfile(os.getcwd() + '/' + userTweetsObj.directory + '/test2.mp4') == True
 
-def testFilesExist():
-    if flag:
-        assert os.path.exists(os.getcwd() + '/' + "musky") == True
-        assert os.path.isfile(os.getcwd() + '/musky/frame0.png') == True
-    else:
-        assert os.path.isfile(os.getcwd() + '/apiExampleVideo/frame0.png') == True
+# def testFilesExist():
+#     if flag:
+#         assert os.path.exists(os.getcwd() + '/' + "musky") == True
+#         assert os.path.isfile(os.getcwd() + '/musky/frame0.png') == True
+#     else:
+#         assert os.path.isfile(os.getcwd() + '/apiExampleVideo/frame0.png') == True
 
 
-def testDirectoryCreateAndDelete():
-    userTweetsObj2 = userTweets(0, 0, 0, 0, "@elizabeth", 4, "hello")
-    userTweetsObj2.makeDirectory()
-    assert os.path.exists(os.getcwd() + '/' + "hello") == True
-    userTweetsObj2.deletePhotosFolder()
-    assert os.path.exists(os.getcwd() + '/' + "hello") == False
+# def testDirectoryCreateAndDelete():
+#     userTweetsObj2 = userTweets(0, 0, 0, 0, "@elizabeth", 4, "hello")
+#     userTweetsObj2.makeDirectory()
+#     assert os.path.exists(os.getcwd() + '/' + "hello") == True
+#     userTweetsObj2.deletePhotosFolder()
+#     assert os.path.exists(os.getcwd() + '/' + "hello") == False
